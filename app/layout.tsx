46571fe0fb0509e4ecebd973fef689bd48bc66bf
Shubham <it21103@glbitm.ac.in>
2024-07-05 00:26:05 +0530

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	metadataBase: new URL("http://localhost:3000"),
+
+	title: "Shubham",
+	authors: {
+		name: "chensokheng",
+	},
+
+	description:
+		"I'm a Front-end Developer and B.Tech (IT) 4th year student.",
+	openGraph: {
+		title: "Shubham",
+		description:
+			"I'm a Front-end Developer and B.Tech (IT) 4th year student.",
+		url: "http://localhost:3000",
+		siteName: "Shubham",
+		images: "/Screenshot(23).png",
+		type: "website",
+	},
+	keywords: ["daily web coding", "chensokheng", "dailywebcoding"],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={spaceGrotesk.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+      </body>
+    </html>
+  );
+}
+
---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from '@/components/Navbar'
+import HeroSection from '@/components/HeroSection'
+import Footer from '@/components/Footer'
+import Skills from '@/components/Skills'
+
+export default function page() {
+  return (
+    <div className='min-h-screen bg-black overflow-hidden'>
+      <div className='dark:bg-black bg-white  dark:bg-grid-white/[0.05]'>
+        <div className='max-w-7xl mx-auto p-5 '>
+          <Navbar />
+          <HeroSection />
+        </div>
+        <div className='h-10 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full'>
+
+        </div>
+        <div className='max-w-7xl mx-auto p-5 mt-20'>
+          <Skills/>
+
+          <Footer/>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
